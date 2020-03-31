aa77ee130d24df65d82d17d49e76d0c881861c1d
sschul1 <62908051+sschul1@users.noreply.github.com>
2020-03-31 20:28:46 +0200

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#This notebook is for the Capstone Project
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
